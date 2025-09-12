ea63796cff65853af1b058cde71ec03963dee457
Matthew Fay <mmfay3@gmail.com>
2025-09-12 05:57:46 -0700

    Add server side session

--- src/lib/auth/session.ts
+++ src/lib/auth/session.ts
@@ -8,13 +8,16 @@ export const SESSION_COOKIE = "sid";
 export type Session = { user_id: string; data: any };
 export type UserRow = { id: string; name: string; email: string };
 
+// get session data
 export async function getSession(): Promise<Session | null> {
     
+    // gets the session sid
     const cookieStore = await cookies();              
     const sid = cookieStore.get(SESSION_COOKIE)?.value;
     
     if (!sid) return null;
 
+    // finds the session in the database based on sid
     const { rows } = await pool.query(
         `select user_id, data
         from auth_sessions
@@ -27,7 +30,7 @@ export async function getSession(): Promise<Session | null> {
     
 }
 
-/** Returns the full user row or null (no auth) */
+// get user data
 export async function getUser(): Promise<UserRow | null> {
     const sess = await getSession();
     if (!sess) return null;
