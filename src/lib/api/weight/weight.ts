535176a11e6004439f6f1b42e7fcadb2fc4e745e
Matthew Fay <mmfay3@gmail.com>
2025-09-20 06:38:47 -0700

    Add weight service

--- src/lib/api/weight/weight.ts
+++ src/lib/api/weight/weight.ts
@@ -14,3 +14,25 @@ export async function fetchWeightTrend(): Promise<WeightPoint[]> {
 
     return res.json();
 }
+
+// posts weight on date, returns array of weights for graph
+export async function addNewWeight(weight: number, dayOfWeight: string): Promise<WeightPoint[]> {
+    const res = await fetch("/api/weight/add", {
+      method: "POST",
+      credentials: "include", // keep cookies/session if you’re using them
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        weight,
+        date: dayOfWeight,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Failed to add new weight: ${res.statusText}`);
+    }
+
+    return res.json();
+
+}
--- src/lib/services/weight.ts
+++ src/lib/services/weight.ts
@@ -15,3 +15,18 @@ export async function getWeightTrend(userId: string, days = 14) {
 	return rows;
 
 }
+
+// adds weight and if old date specified it updates it, then returns trend to front end
+export async function addWeight(userId: string, dateOfWeight: Date, weight: number) {
+
+	await pool.query(
+		`insert into weight (user_id, measured_at, weight, unit)
+		values ($1, $2::date, $3, 'lb')
+		ON CONFLICT (user_id, measured_at)
+		DO UPDATE SET
+  			weight = EXCLUDED.weight;`,
+		[userId, dateOfWeight, weight]
+	);
+	return getWeightTrend(userId);
+
+}
